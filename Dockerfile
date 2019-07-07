eb6cbe059caa1614bb890879e962043e27b8c82b
nannany <ymym1990ymym@gmail.com>
2019-07-07 18:19:27 +0900

    修正する

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:8-jdk-alpine
+RUN mkdir -p /usr/opt/service
+COPY target/*.jar /usr/opt/service/service.jar
+EXPOSE 8080
+ENTRYPOINT exec java -jar /usr/opt/service/service.jar
--- src/main/java/nannany/sample/actuator/ActuatorApplication.java
+++ src/main/java/nannany/sample/actuator/ActuatorApplication.java
@@ -6,12 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActuatorApplication {
 
-	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(ActuatorApplication.class, args);
-		while (true) {
-			System.out.println("Hello World!");
-			Thread.sleep(1997L);
-		}
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
 
 }
